0241f22a3602f0be008789800dc10663827a473f
veerendra <24B11CS251@adityauniversity.com>
2026-07-30 09:35:22 +0530

    Fix JWT cookie for production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,19 +1,25 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, 
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
     {
-        expiresIn: '7d'
-    });
+      expiresIn: "7d",
+    }
+  );
 
-    res.cookie('jwt', token, 
-    {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        httpOnly: true,  // cookie cannot be accessed by client side scripts
-        sameSite: 'strict', // cookie will only be sent to the same site
-        secure: process.env.NODE_ENV !== "development" // cookie will only be sent over https in production
-    });
-    
-    return token;
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 
-}
+    httpOnly: true,
+
+    // Required for Vercel frontend + Render backend
+    sameSite: "none",
+
+    // Required when sameSite is none
+    secure: true,
+  });
+
+  return token;
+};

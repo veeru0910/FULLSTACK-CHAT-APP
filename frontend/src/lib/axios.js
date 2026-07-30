1f3f8c9c40ad338e72499dcfc73248d4415c4d4e
veerendra <24B11CS251@adityauniversity.com>
2026-07-30 09:04:28 +0530

    Debug VITE_API_URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
+
+console.log("MODE:", import.meta.env.MODE);
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5000/api': "/api",
-    withCredentials: true,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true,
 });
 
 export default axiosInstance;

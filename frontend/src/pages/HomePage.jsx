51bae449806a9a5cf68536f8426d39de28384cd2
veerendra <24B11CS251@adityauniversity.com>
2026-07-30 10:53:33 +0530

    Improve mobile chat fullscreen layout

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,20 +5,103 @@ import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
+
   const { selectedUser } = useChatStore();
 
-  return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
 
-            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+  return (
+
+    <div className="h-screen bg-base-200">
+
+
+      <div className="
+        flex 
+        items-center 
+        justify-center
+        px-0
+        sm:px-4
+      ">
+
+
+        <div className="
+          bg-base-100 
+          w-full 
+          h-screen
+          sm:h-[calc(100vh-2rem)]
+          sm:max-w-6xl
+          sm:rounded-lg
+          shadow-xl
+        ">
+
+
+          <div className="
+            flex 
+            h-full 
+            overflow-hidden
+            sm:rounded-lg
+          ">
+
+
+
+            {/* Hide sidebar on mobile when chatting */}
+
+            <div
+              className={`
+                ${
+                  selectedUser
+                  ? "hidden sm:block"
+                  : "block"
+                }
+              `}
+            >
+
+              <Sidebar />
+
+            </div>
+
+
+
+
+
+            {/* Chat Area */}
+
+            <div
+              className={`
+                flex-1
+                ${
+                  selectedUser
+                  ? "block"
+                  : "hidden sm:block"
+                }
+              `}
+            >
+
+              {
+                !selectedUser
+                ?
+                <NoChatSelected />
+                :
+                <ChatContainer />
+              }
+
+            </div>
+
+
+
           </div>
+
+
         </div>
+
+
       </div>
+
+
     </div>
+
   );
+
 };
+
+
 export default HomePage;
